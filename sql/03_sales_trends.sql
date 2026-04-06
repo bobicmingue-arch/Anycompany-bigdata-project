2c29b2a95c81f5de23a150e1f0bd48311e1fcf6f
bobicmingue-arch <bobicmingue@gmail.com>
2026-04-06 16:12:18 +0200

    Update 03_sales_trends.sql

--- sql/03_sales_trends.sql
+++ sql/03_sales_trends.sql
@@ -1,1 +1,9 @@
 
+-- Analyse de l'évolution des ventes dans le temps
+
+SELECT
+  YEAR(transaction_date) AS year,
+  SUM(amount) AS total_sales
+FROM SILVER.financial_transactions_clean
+GROUP BY YEAR(transaction_date)
+ORDER BY year;
