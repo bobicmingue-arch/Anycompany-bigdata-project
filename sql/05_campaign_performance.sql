0c8d874c38111667703950bf14dc807aa824e517
bobicmingue-arch <bobicmingue@gmail.com>
2026-04-06 16:13:44 +0200

    Update 05_campaign_performance.sql

--- sql/05_campaign_performance.sql
+++ sql/05_campaign_performance.sql
@@ -1,1 +1,11 @@
 
+-- Analyse de la performance des campagnes marketing
+
+SELECT
+  campaign_name,
+  campaign_type,
+  budget,
+  reach,
+  conversion_rate
+FROM SILVER.marketing_campaigns_clean
+ORDER BY conversion_rate DESC;
