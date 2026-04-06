4e945996d4a49a1473458001ee37a08d62cd1dfb
bobicmingue-arch <bobicmingue@gmail.com>
2026-04-06 16:09:25 +0200

    Update 01_Load_data.sql

--- sql/01_Load_data.sql
+++ sql/01_Load_data.sql
@@ -1,1 +1,17 @@
+-- ============================================
+-- 01_Load_data.sql
+-- Phase 1 : Préparation et chargement des données
+-- ============================================
 
+USE DATABASE ANYCOMPANY_LAB;
+USE SCHEMA BRONZE;
+
+-- Chargement des données clients
+COPY INTO customer_demographics
+FROM @PUBLIC.food_beverage_stage/customer_demographics.csv
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1);
+
+-- Chargement des transactions
+COPY INTO financial_transactions
+FROM @PUBLIC.food_beverage_stage/financial_transactions.csv
+FILE_FORMAT = (TYPE = 'CSV' SKIP_HEADER = 1 FIELD_OPTIONALLY_ENCLOSED_BY = '"');
