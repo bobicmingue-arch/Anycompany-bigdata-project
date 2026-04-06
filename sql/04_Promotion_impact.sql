319597c52d3bd003b551c516daae6396401b7ecd
bobicmingue-arch <bobicmingue@gmail.com>
2026-04-06 16:13:02 +0200

    Update 04_Promotion_impact.sql
    Performance marketing

--- sql/04_Promotion_impact.sql
+++ sql/04_Promotion_impact.sql
@@ -1,1 +1,14 @@
 
+-- Analyse de l'impact des promotions sur les ventes
+
+SELECT
+  CASE
+    WHEN p.promotion_id IS NOT NULL THEN 'With Promotion'
+    ELSE 'Without Promotion'
+  END AS promotion_status,
+  SUM(t.amount) AS total_sales
+FROM SILVER.financial_transactions_clean t
+LEFT JOIN SILVER.promotions_clean p
+  ON t.region = p.region
+ AND t.transaction_date BETWEEN p.start_date AND p.end_date
+GROUP BY promotion_status;
