e80b1766669cf04d9c55e12977681c258df31def
bobicmingue-arch <bobicmingue@gmail.com>
2026-04-07 15:53:19 +0200

    Create marketing_roi.py

---
+++ streamlit/marketing_roi.py
@@ -0,0 +1,11 @@
+"""
+Marketing ROI - Streamlit
+
+Dashboard focusing on the effectiveness of marketing campaigns.
+"""
+
+import streamlit as st
+
+st.title("Marketing ROI Dashboard")
+
+st.write("Overview of campaign performance and return on investment.")
